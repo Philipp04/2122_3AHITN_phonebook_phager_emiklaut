88cdb064237ac6f38cfd508d16da20a9058fbb69
Philipp04 <phager@htl-steyr.ac.at>
2022-03-03 14:44:03 +0100

    Date and Person completed

--- src/com/company/Date.java
+++ src/com/company/Date.java
@@ -1,4 +1,50 @@
 package com.company;
 
 public class Date {
+    private int day;
+    private int mon;
+    private int year;
+
+    Date(int m, int d, int y){
+        mon = m;
+        day = d;
+        year = y;
+    }
+
+    Date(String date){
+        String[] parts = date.split(".");
+        day = Integer.parseInt(parts[0]);
+        mon = Integer.parseInt(parts[1]);
+        year = Integer.parseInt(parts[2]);
+    }
+
+    public int getDay(){
+        return day;
+    }
+
+    public int getMon(){
+        return mon;
+    }
+
+    public int getYear(){
+        return year;
+    }
+
+    public boolean isLeapYear(){
+        boolean r = false;
+        if(year /4 == 0) {
+            r = true;
+            if (year / 100 == 0 && year / 400 != 0) {
+                r = false;
+            }
+        }
+        return r;
+
+    }
+
+    public boolean isValid(Date d){
+
+
+        return false;
+    }
 }
--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -4,5 +4,24 @@ public class Person {
     private String name;
     private String givenname;
     private String nickname;
-    private Date birthday
+    private Date birthday;
+    private PhoneNumber phone;
+
+    Person (String n, String gn, Date bd, PhoneNumber p){
+        name = n;
+        givenname = gn;
+        birthday = bd;
+        phone = p;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", givenname='" + givenname + '\'' +
+                ", nickname='" + nickname + '\'' +
+                ", birthday=" + birthday +
+                ", phone=" + phone +
+                '}';
+    }
 }
