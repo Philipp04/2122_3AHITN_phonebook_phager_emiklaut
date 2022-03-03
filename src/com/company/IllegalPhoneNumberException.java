5cc5368ead2bf30cca17993afa108fdf2b515c69
emiklaut <95867791+emiklaut@users.noreply.github.com>
2022-03-03 14:49:07 +0100

    IllegalDateException und IllegalPhoneNumberException

--- src/com/company/IllegalPhoneNumberException.java
+++ src/com/company/IllegalPhoneNumberException.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class IllegalPhoneNumberException {
     public int type;
-    public final static int COUNTRY_ILLEGAL = 1232112;
+    public final static int COUNTRY_ILLEGAL = 1;
     public final static int AREA_ILLEGAL = COUNTRY_ILLEGAL + 1;
     public final static int NUMBER_ILLEGAL = AREA_ILLEGAL + 1;
     public final static int STRING_FORMAT_ILLEGAL = NUMBER_ILLEGAL + 1;
@@ -10,5 +10,9 @@ public class IllegalPhoneNumberException {
 
     public IllegalPhoneNumberException(int type) {
         this.type = type;
+
+        if (type == 1){
+            System.out.println("Illegales Land");
+        }else if ()
     }
 }
