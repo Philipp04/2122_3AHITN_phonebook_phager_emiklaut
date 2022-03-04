8fed587618e4e6499648adca5df22a57ca223cb9
emiklaut <95867791+emiklaut@users.noreply.github.com>
2022-03-04 08:51:59 +0100

    Kommentare

--- src/com/company/StreamOperation.java
+++ src/com/company/StreamOperation.java
@@ -3,6 +3,17 @@ package com.company;
 import java.io.Reader;
 import java.io.Writer;
 
+/**
+ * Streamoperation ist verantwortlich ein File zu schreiben und
+ * ein File zu lesen.
+ *
+ * @author emiklaut
+ *
+ * @version  1.0
+ */
+
+
+
 public class StreamOperation {
     public void ToStream(Writer w, Person p){
 
